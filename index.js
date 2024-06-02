4a2a269dd3d30c6efb7129b0dfdb79143da9ed6d
Doug <doug.a.crawford@gmail.com>
2024-06-02 15:55:23 -0700

    Deliver missed messages after client disconnects

--- index.js
+++ index.js
@@ -3,10 +3,27 @@ import { createServer } from "node:http";
 import { fileURLToPath } from "node:url";
 import { dirname, join } from "node:path";
 import { Server } from "socket.io";
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
+
+const db = await open({
+  filename: "chat.db",
+  driver: sqlite3.Database,
+});
+
+await db.exec(`
+    create table if not exists messages (
+        id integer primary key autoincrement,
+        client_offset text unique,
+        content text
+    );
+`);
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  connectionStateRecovery: {},
+});
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -14,12 +31,34 @@ app.get("/", (req, res) => {
   res.sendFile(join(__dirname, "index.html"));
 });
 
-io.on("connection", (socket) => {
+io.on("connection", async (socket) => {
   console.log("a user connected");
 
-  socket.on("chat message", (msg) => {
-    console.log("message: " + msg);
+  //   socket.broadcast.emit("hi"); // can't see this in server logs? i don't understand yet
+
+  socket.on("chat message", async (msg) => {
+    let result;
+    try {
+      result = await db.run(`INSERT INTO messages (content) VALUES (?)`, msg);
+    } catch (e) {
+      return;
+    }
+    io.emit("chat message", msg, result.lastID);
   });
+
+  if (!socket.recovered) {
+    try {
+      await db.each(
+        "SELECT id, content FROM messages WHERE id > ?",
+        [socket.handshake.auth.serverOffset || 0],
+        (_err, row) => {
+          socket.emit("chat message", row.content, row.id);
+        }
+      );
+    } catch (e) {
+      console.log("something went wrong: ", e);
+    }
+  }
 
   socket.on("disconnect", () => {
     console.log("user disconnected");
